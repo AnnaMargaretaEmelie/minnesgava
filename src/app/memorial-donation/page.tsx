50f27aec0f79e45bd4175880dac02c766c03ce72
Anna <anna.viklund1@medieinstitutet.se>
2025-11-21 10:25:25 +0100

    refactor: restore COPY_QUERYResult typing for memorial copy fetch

--- src/app/memorial-donation/page.tsx
+++ src/app/memorial-donation/page.tsx
@@ -1,9 +1,10 @@
 import { COPY_QUERY } from "@/sanity/lib/queries";
 import { client } from "@/sanity/lib/client";
 import { MemorialPageSection } from "./sections/MemorialPageSection/MemorialPageSection";
+import { COPY_QUERYResult } from "../../../sanity/types";
 
 export default async function MemorialDonationPage() {
-  const copy = await client.fetch(COPY_QUERY);
+  const copy = await client.fetch<COPY_QUERYResult>(COPY_QUERY);
   const memorialPageCopy = copy?.memorialCard?.introSection;
 
   if (!memorialPageCopy) {
