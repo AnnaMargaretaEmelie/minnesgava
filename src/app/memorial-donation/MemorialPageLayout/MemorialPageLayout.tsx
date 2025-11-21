6a4e735389d74a08ae8d25bb9b081c92c5bcac60
Anna <anna.viklund1@medieinstitutet.se>
2025-11-21 15:48:05 +0100

    refactor: use shared accordion components in memorial layout

--- src/app/memorial-donation/MemorialPageLayout/MemorialPageLayout.tsx
+++ src/app/memorial-donation/MemorialPageLayout/MemorialPageLayout.tsx
@@ -1,24 +1,45 @@
 import type { MemorialPageLayoutProps } from "./MemorialPageLayout.types";
 import { MemorialPageSection } from "../sections/MemorialPageSection/MemorialPageSection";
 import styles from "./MemorialPageLayout.module.scss";
+import { AccordionItem } from "@/app/components/accordion/accordionItem";
+import { AccordionRoot } from "@/app/components/accordion/accordion";
 
 export function MemorialPageLayout({ copy }: MemorialPageLayoutProps) {
   return (
     <div className={styles.layout}>
       <section className={styles.hero}>HeroSection</section>
       <section className={styles.steps}>
-        <section className={styles.step}>
-          <MemorialPageSection copy={copy} />
-        </section>
-        <section className={styles.step}>
-          <h2>Gåvobelopp</h2>
-        </section>
-        <section className={styles.step}>
-          <h2>Kontaktuppgifter</h2>
-        </section>
-        <section className={styles.step}>
-          <h2>Betalsätt</h2>
-        </section>
+        <AccordionRoot>
+          <AccordionItem
+            value="memorial-card-step"
+            title="1. Minnesblad"
+            className={styles.step}
+          >
+            <MemorialPageSection copy={copy} />
+          </AccordionItem>
+          <AccordionItem
+            value="amount-step"
+            title="2. Gåvobelopp"
+            className={styles.step}
+          >
+            <p>Kommande innehåll</p>
+          </AccordionItem>
+          <AccordionItem
+            value="donor-contact-step"
+            title="3. Kontaktuppgifter"
+            className={styles.step}
+          >
+            <p>Kommande innehåll</p>
+          </AccordionItem>
+
+          <AccordionItem
+            value="payment-step"
+            title="4. Betalsätt"
+            className={styles.step}
+          >
+            <p>Kommande innehåll</p>
+          </AccordionItem>
+        </AccordionRoot>
       </section>
     </div>
   );
